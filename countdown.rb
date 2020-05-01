eedb706e196dce2c4878fddcc8fd3f9952432d66
Spencer Cambern <cambern134@gmail.com>
2020-05-01 08:59:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(seconds_to_midnight)
+seconds_to_midnight = 1
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} is less than 10"
+    seconds_to_midnight -= 1
+  end
+
 end
