6e319d0287683db8f5c69c90651d220c38b0e560
Spencer Cambern <cambern134@gmail.com>
2020-05-01 09:04:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)"
+    puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
   end
 "HAPPY NEW YEAR!"
@@ -10,7 +10,8 @@ end
 
 def countdown_with_sleep(seconds_to_midnight)
   while seconds_to_midnight > 10
-    puts "#{seconds_to_midnight} SECOND(S)"
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
     seconds_to_midnight -= 1
   end
   "HAPPY NEW YEARS!"
